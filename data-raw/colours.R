d3532df74c65c283cff8c0b7e785a29df42cfa6f
Paul McAdam <paul-macbook-air@pauls-air-2.lan>
2020-08-23 21:14:53 +0100

    Add 19/20 data object

--- data-raw/colours.R
+++ data-raw/colours.R
@@ -1,4 +1,4 @@
-palette_data <- readr::read_tsv("./data-raw/colours.tsv")
+palette_data <- readr::read_tsv("colours.tsv")
 team_fills <- palette_data[["Fill"]]
 team_colours <- palette_data[["Colour"]]
 names(team_fills) <- names(team_colours) <- palette_data[["Team"]]
---
+++ data-raw/fpl_1920.R
@@ -0,0 +1,3 @@
+fpl_1920_data <- match_players()
+
+usethis::use_data(fpl_1920_data)
