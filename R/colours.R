e3cd7d9a28b91bc7e02a8addd333b0d590f73e15
Paul McAdam <paul-macbook-air@pauls-air-2.lan>
2020-08-23 21:04:33 +0100

    Add data

--- R/colours.R
+++ R/colours.R
@@ -1,4 +1,7 @@
-palette_data <- readr::read_tsv("./data-raw/colours")
+palette_data <- readr::read_tsv("./data-raw/colours.tsv")
 team_fills <- palette_data[["Fill"]]
 team_colours <- palette_data[["Colour"]]
 names(team_fills) <- names(team_colours) <- palette_data[["Team"]]
+
+# usethis::use_data(team_fills)
+# usethis::use_data(team_colours)
